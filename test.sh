de9c8a3d139bc166a3b2147882ddd3a263133744
narayan-paudel <eknpaudel@gmail.com>
2022-07-14 11:44:22 -0500

    folder change

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ebin=7.0
-rad=$(python -c "print(800+(int($ebin)-5)*300 + (2*(int($ebin)-5)//3)*300 + ((int($ebin)-5)//3)*300)")
-echo rad $rad
--- testList.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-A = ["sam","jam","tam"]
-print("{0}".format(str(*A)))
